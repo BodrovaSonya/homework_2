8753d5932a813cb357ab576939e6c6a9c478478b
BodrovaSonya <bodrova_sonya@mail.ru>
2017-12-06 13:36:31 +0300

    Task 4 is updated

--- fourth_task/src/main/java/Main.java
+++ fourth_task/src/main/java/Main.java
@@ -8,24 +8,45 @@ import java.util.Random;
 public class Main {
     public static final int ARRAY_LENGTH = 20;
     public static void main(String[] args) {
+        int indexMaxNegative, indexMinPositive;
+        boolean negative = true, positive = true;
         int randNumbers[] = new int[ARRAY_LENGTH];
         for (int i = 0; i < ARRAY_LENGTH; i++){
             Random random = new Random();
             randNumbers[i] = -10 + random.nextInt(11+10);
             System.out.println(i + ") " + randNumbers[i]);
         }
-        System.out.println("Max number is: " + findMaxnegative(randNumbers));
+        indexMaxNegative = findMaxNegativeIndex(randNumbers);
+        System.out.println("Max negative number is " + randNumbers[indexMaxNegative]);
+        if (indexMaxNegative == 1000) {
+            System.out.println("There are no negative numbers");
+            negative = false;
+        }
+        indexMinPositive = findMinPositive(randNumbers);
+        System.out.println("Min positive number is " + randNumbers[indexMinPositive]);
+        if (indexMinPositive == 1000) {
+            System.out.println("There are no positive numbers");
+            positive = false;
+        }
+
+        if (positive && negative) {
+            swap(randNumbers,indexMaxNegative,indexMinPositive);
+            for (int i = 0; i < ARRAY_LENGTH; i++) {
+                System.out.println(i + ") " + randNumbers[i]);
+            }
+        }
+        else System.out.println("There is no positive or negative number, nothing to swap");
     }
 
 
     /**
      *
-     * @param array to find max element
-     * @return max number from array
+     * @param array to find max negative element
+     * @return max negative number from array
      */
-    public static int findMaxnegative(int array[]){
+    public static int findMaxNegativeIndex(int array[]){
         int maxNegative = -1000;
-        int indexMaxNegative = 0;
+        int indexMaxNegative = 1000;
         for (int i = 0; i < array.length; i++) {
                 if (array[i] < 0) {
                     maxNegative = array[i];
@@ -41,11 +62,48 @@ public class Main {
                 }
             }
         }
-        else {
-            System.out.println("There are no negative numbers");
-            return -30;
-        }
         return indexMaxNegative;
     }
 
+    /**
+     *
+     * @param array to find min positive element
+     * @return min positive number from array
+     */
+    public static int findMinPositive(int array[]){
+        int minPositive = 1000;
+        int indexMinPositive = 1000;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] > 0) {
+                minPositive = array[i];
+                indexMinPositive = i;
+                break;
+            }
+        }
+
+        if (minPositive != 1000) {
+            for (int i = 0; i < array.length; i++) {
+                if ((array[i] > 0) && (array[i] < minPositive)) {
+                    minPositive = array[i];
+                    indexMinPositive = i;
+                }
+            }
+        }
+        return indexMinPositive;
+    }
+
+
+    /**
+     *
+     * @param array where will change max negative and min positive
+     * @param indexMaxNeg is index of max negative number
+     * @param indexMinPos is index of min positive number
+     * @return updated array
+     */
+    public static int [] swap (int array[], int indexMaxNeg, int indexMinPos) {
+        int tmp = array[indexMaxNeg];
+        array[indexMaxNeg] = array[indexMinPos];
+        array[indexMinPos] = tmp;
+        return array;
+    }
 }
